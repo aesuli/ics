5f0c9fa8555103165117b4f626ed971756637cc4
Andrea Esuli <andrea.esuli@isti.cnr.it>
2016-01-26 17:01:43 +0100

    improved feature extraction

--- classifier/rich_analyzer.py
+++ classifier/rich_analyzer.py
@@ -4,9 +4,9 @@ urlre = re.compile(r'http[s]{,1}://[^ ]+')
 mentionre = re.compile(r'@[\w]+')
 hashre = re.compile(r'#[\w]+')
 emotre = re.compile(
-    r'(:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)')
+        r'(:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)')
 featre = re.compile(
-    r'(http[s]{,1}://[^ ]+|[\w\-]+|#\w+|@\w+|\:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[;:,\!\.\?]|$)')
+        r'(http[s]{,1}://[^ ]+|[\w\-]+|#\w+|@\w+|\:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[;:,\!\.\?]|$)')
 
 
 def clean_html(html):
@@ -21,19 +21,32 @@ def ngrams(items, n, prefix):
     return [prefix + '_'.join(items[start:start + n]) for start in range(0, len(items) - n + 1)]
 
 
-def rich_analyzer(doc):
+def rich_analyzer(doc, word_ngrams=None, char_ngrams=None, stopwords=None):
+    if word_ngrams is None:
+        word_ngrams = list()
+    if char_ngrams is None:
+        char_ngrams = list()
+    if stopwords is None:
+        stopwords = set()
+    else:
+        stopwords = set(stopwords)
+
     doc = clean_html(doc)
     output = list()
     output.extend(featre.findall(doc))
-    output = [x for x in output if len(x) > 1]
+    output = [x for x in output if len(x) > 1 and not x in stopwords]
 
-    word_bigrams = ngrams(output, 2, '_WBG_')
-    word_trigrams = ngrams(output, 3, '_WTG_')
-    output.extend(word_bigrams)
-    output.extend(word_trigrams)
+    if word_ngrams is None:
+        word_ngrams = list()
+    ngm = list()
+    for n in word_ngrams:
+        ngm.extend(ngrams(output, n, '_W%iG_' % n))
+    output.extend(ngm)
 
-    char_trigrams = ngrams(doc, 3, '_CTG_')
-    output.extend(char_trigrams)
+    ngm = list()
+    for n in char_ngrams:
+        ngm.extend(ngrams(doc, n, '_C%iG_' % n))
+    output.extend(ngm)
 
     for alttag, regex in [('_URL', urlre), ('_MENTION', mentionre), ('_HASHTAG', hashre), ('_EMOTICON', emotre)]:
         output.extend([alttag for _ in regex.findall(doc)])
@@ -42,5 +55,5 @@ def rich_analyzer(doc):
 
 if __name__ == '__main__':
     example = 'nel @me_2zzo y < x http://www.es.com/ <br /> <p> <a href=\'ff\'> ddd </a>  :del: https://wd.d cammin ;) e < r  r >= d f): di #nos22tr-fa, #vita.'
-    output = rich_analyzer(example)
+    output = rich_analyzer(example, [2, 3], [2, 3])
     print(output)
