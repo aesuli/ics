af0d44f552ddacef4cac09eea2683799d44eb7d8
Andrea Esuli <andrea.esuli@isti.cnr.it>
2017-06-16 17:37:03 +0200

    added config and arguments to script

--- webservice/web_service.py
+++ webservice/web_service.py
@@ -1,4 +1,7 @@
+import sys
+
 import cherrypy
+from configargparse import ArgParser
 
 import webservice.web_classifier_client as client
 from webservice.background_processor import BackgroundProcessor
@@ -9,16 +12,27 @@ from webservice.web_dataset_collection import WebDatasetCollection
 __author__ = 'Andrea Esuli'
 
 if __name__ == "__main__":
-    db_connection_string = 'postgresql://ics:ics@localhost:5432/ics'
-    with BackgroundProcessor(db_connection_string) as background_processor, WebClassifierClient(
-            db_connection_string) as app, WebClassifierCollection(
-        db_connection_string, background_processor) as classifier_service, WebDatasetCollection(
-        db_connection_string, background_processor) as dataset_service:
+    parser = ArgParser()
+    parser.add_argument('-c', '--config', help='configuration file', is_config_file=True)
+    parser.add_argument('--db_connection_string', required=True)
+    parser.add_argument('--app_path', help='server path of the web client app', type=str, default='/')
+    parser.add_argument('--classifier_path', help='server path of the classifier web service', type=str,
+                        default='/service/classifiers')
+    parser.add_argument('--dataset_path', help='server path of the dataset web service', type=str,
+                        default='/service/datasets')
+    parser.add_argument('--processor_path', help='server path of the background processor web service', type=str,
+                        default='/service/jobs')
+    args = parser.parse_args(sys.argv[1:])
+
+    with BackgroundProcessor(args.db_connection_string) as background_processor, WebClassifierClient(
+            args.db_connection_string) as app, WebClassifierCollection(
+        args.db_connection_string, background_processor) as classifier_service, WebDatasetCollection(
+        args.db_connection_string, background_processor) as dataset_service:
         background_processor.start()
-        cherrypy.tree.mount(app, '/', config=client.config)
-        cherrypy.tree.mount(classifier_service, '/service/classifiers')
-        cherrypy.tree.mount(dataset_service, '/service/datasets')
-        cherrypy.tree.mount(background_processor, '/service/jobs')
+        cherrypy.tree.mount(app, args.app_path, config=client.config)
+        cherrypy.tree.mount(classifier_service, args.classifier_path)
+        cherrypy.tree.mount(dataset_service, args.dataset_path)
+        cherrypy.tree.mount(background_processor, args.processor_path)
         cherrypy.engine.subscribe('stop', background_processor.stop)
         cherrypy.engine.start()
         cherrypy.engine.block()
