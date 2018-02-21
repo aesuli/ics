3a6e6eb9150b78a28ef3cdc8c00c546c933df4f9
Andrea Esuli <andrea.esuli@isti.cnr.it>
2018-02-21 12:18:09 +0100

    implementing ip tracker

--- webservice/start_all.py
+++ webservice/start_all.py
@@ -4,13 +4,13 @@ import sys
 import cherrypy
 from configargparse import ArgParser
 
-from webservice.auth_controller_service import AuthControllerService, must_be_logged_in_or_redirect, \
-    must_be_logged_in, any_of
+from webservice.auth_controller_service import any_of, enable_controller_service
 from webservice.background_processor_service import BackgroundProcessor
 from webservice.classifier_collection_service import ClassifierCollectionService
 from webservice.dataset_collection_service import DatasetCollectionService
-from webservice.ip_controller_service import ip_rate_limit
+from webservice.ip_controller_service import ip_rate_limit, IPControllerService
 from webservice.jobs_service import JobsService
+from webservice.user_controller_service import UserControllerService, must_be_logged_in_or_redirect, must_be_logged_in
 from webservice.web_client import WebClient
 
 __author__ = 'Andrea Esuli'
@@ -27,7 +27,9 @@ if __name__ == "__main__":
     parser.add_argument('--host', help='host server address', type=str, required=True)
     parser.add_argument('--port', help='host server port', type=int, required=True)
     parser.add_argument('--client_path', help='server path of the web client app', type=str, required=True)
-    parser.add_argument('--auth_path', help='server path of the auth web service', type=str, required=True)
+    parser.add_argument('--hourly_limit', help='hourly request limit', type=int, required=True)
+    parser.add_argument('--user_auth_path', help='server path of the user auth web service', type=str, required=True)
+    parser.add_argument('--ip_auth_path', help='server path of the ip auth web service', type=str, required=True)
     parser.add_argument('--classifier_path', help='server path of the classifier web service', type=str, required=True)
     parser.add_argument('--dataset_path', help='server path of the dataset web service', type=str, required=True)
     parser.add_argument('--jobs_path', help='server path of the jobs web service', type=str,
@@ -36,17 +38,19 @@ if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
 
     with BackgroundProcessor(args.db_connection_string) as background_processor, \
-            WebClient(args.db_connection_string, args.media_dir, args.auth_path, args.classifier_path,
-                      args.dataset_path,
-                      args.jobs_path, args.name) as client, \
+            WebClient(args.db_connection_string, args.media_dir, args.user_auth_path, args.classifier_path,
+                      args.dataset_path, args.jobs_path, args.name) as client, \
             ClassifierCollectionService(args.db_connection_string, args.data_dir) as classifier_service, \
             DatasetCollectionService(args.db_connection_string, args.data_dir) as dataset_service, \
             JobsService(args.db_connection_string) as jobs_service, \
-            AuthControllerService(args.db_connection_string, args.min_password_length) as auth_controller:
+            UserControllerService(args.db_connection_string, args.min_password_length) as user_auth_controller, \
+            IPControllerService(args.db_connection_string, args.hourly_limit) as ip_auth_controller:
         background_processor.start()
 
         cherrypy.server.socket_host = args.host
         cherrypy.server.socket_port = args.port
+
+        enable_controller_service()
 
         conf_client = {
             '/': {
@@ -67,7 +71,8 @@ if __name__ == "__main__":
         cherrypy.tree.mount(client, args.client_path, config={**client.get_config(), **conf_client})
         cherrypy.tree.mount(classifier_service, args.classifier_path, config=conf_service)
         cherrypy.tree.mount(dataset_service, args.dataset_path, config=conf_service)
-        cherrypy.tree.mount(auth_controller, args.auth_path, config=conf_service)
+        cherrypy.tree.mount(user_auth_controller, args.user_auth_path, config=conf_service)
+        cherrypy.tree.mount(ip_auth_controller, args.ip_auth_path, config=conf_service)
         cherrypy.tree.mount(jobs_service, args.jobs_path, config=conf_service)
 
         cherrypy.engine.subscribe('stop', background_processor.stop)
